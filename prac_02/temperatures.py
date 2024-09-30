392db4a0abcba22d610310fbef055ba1b0c39e95
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-09-30 09:44:37 +0800

    Initial Commit

---
+++ prac_02/temperatures.py
@@ -0,0 +1,18 @@
+"""
+CP1404 - Practical
+Temperature Conversion Program
+
+def celsius_to_fahrenheit()
+    celsius * 9.0 / 5.0 + 32
+
+def fahrenheit_to_celsius()
+    (fahrenheit - 32) * 5.0 / 9.0
+
+def main()
+    header
+    menu
+        Option 1: celsius to fahrenheit
+        Option 2: fahrenheit to celsius
+        Invalid option
+"""
+
