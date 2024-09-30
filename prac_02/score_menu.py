539422ae7952195743f417559c2e8e3450bfdb84
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-09-30 10:01:43 +0800

    Initial Commit

---
+++ prac_02/score_menu.py
@@ -0,0 +1,29 @@
+"""
+CP1404 - Practical 2
+
+def main()
+    menu
+        Option 1: Get valid score
+        Option 2: Print result
+        Option 3: Show stars
+        Option 4: Quit
+        Invalid Option
+
+def get_valid_score()
+    input score
+    while score < minimum or > maximum
+        print invalid message
+        input score
+
+def determine_result
+    if score >= 90
+        return excellent
+    if score >= 50
+        return passable
+    else
+        return bad
+
+def print_stars()
+    print score * "*"
+"""
+
