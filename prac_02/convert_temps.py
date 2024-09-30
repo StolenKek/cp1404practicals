cb2b49c17d3b3bb01e45aac097df742fde9db035
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-09-30 10:56:30 +0800

    Initial Commit

---
+++ prac_02/convert_temps.py
@@ -0,0 +1,23 @@
+"""
+CP1404 - Practical
+Convert Fahrenheit temperatures to Celsius and save the results.
+
+def generate_temperatures()
+    open file using write
+    for i in range(number_of_temperatures):
+        temperature = random.uniform(-200, 200)
+        file write
+
+def fahrenheit_to_celsius(fahrenheit):
+   (fahrenheit - 32) * 5.0 / 9.0
+
+def read_temperatures()
+    open file using read
+    line strip in float
+
+def write_temperatures()
+    open file using write
+
+def main()
+"""
+
