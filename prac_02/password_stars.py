6393858ed964da0d205d731e52906e6509767fbf
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-09-30 09:37:08 +0800

    Added password validation (length requirement)

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,14 +3,19 @@ CP1404 - Practical 2
 Password Check with Functions
 """
 
+PASSWORD_LENGTH = 10
+
 def main():
     """Main function to drive the password check program."""
     user_password = get_password()
     print_asterisks(user_password)
 
 def get_password():
-    """Prompt the user for a password."""
-    password = input(f"Enter a password: ")
+    """Prompt the user for a password and ensure it meets the required length."""
+    password = input(f"Enter a password (at least {PASSWORD_LENGTH} characters): ")
+    while len(password) < PASSWORD_LENGTH:
+        print(f"Password must be at least {PASSWORD_LENGTH} characters long.")
+        password = input("Enter password: ")
     return password
 
 def print_asterisks(password):
