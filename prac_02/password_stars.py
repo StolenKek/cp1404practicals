7b2deaf72491ae50b088880725aef3343ae6fcc1
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-09-30 09:34:21 +0800

    Refactored code

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,12 +1,21 @@
-""""
+"""
 CP1404 - Practical 2
 Password Check with Functions
-
 """
 
-user_password = str(input("Enter password: "))
+def main():
+    """Main function to drive the password check program."""
+    user_password = get_password()
+    print_asterisks(user_password)
 
-letter_count = len(user_password)
+def get_password():
+    """Prompt the user for a password."""
+    password = input(f"Enter a password: ")
+    return password
 
-print(letter_count * "*")
+def print_asterisks(password):
+    """Print asterisks equal to the length of the password."""
+    print("*" * len(password))
 
+if __name__ == "__main__":
+    main()
