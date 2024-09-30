88b0c3b310f3ed34be94eacadfb986b7a10e8ebf
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-09-30 09:31:40 +0800

    Initial Commit

---
+++ prac_02/password_stars.py
@@ -0,0 +1,12 @@
+""""
+CP1404 - Practical 2
+Password Check with Functions
+
+"""
+
+user_password = str(input("Enter password: "))
+
+letter_count = len(user_password)
+
+print(letter_count * "*")
+
