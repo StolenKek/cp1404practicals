0f818e6d3d86a6e030b0ffc5b1a82dbc11d45339
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-06 15:09:53 +0800

    Added code that creates name.txt and writes into it, reads name.txt and prints it, and reads numbers.txt and print the sum of the first two numbers

--- prac_03/files.py
+++ prac_03/files.py
@@ -6,7 +6,8 @@ readline()
 readlines()
 for line in file
 
-1) Write code that asks the user for their name, then opens a file called name.txt and writes that name to it. Use open and close for this question.
+1) Write code that asks the user for their name, then opens a file called name.txt and writes that name to it.
+    Use open and close for this question.
 
 2) In the same file, but as if it were a separate program, write code that opens "name.txt" and reads the name (as above) then prints (note the exact output),
     Hi Bob! (or whatever the name is in the file).
@@ -26,4 +27,20 @@ for line in file
     Use with instead of open and close for this question.
 """
 
+# 1) Code that asks for name, opens name.txt, then writes name in it
+out_file = open("name.txt", "w")
+name = input("Enter name: ")
+print(name, file=out_file)
+out_file.close()
 
+# 2) Code that opens name.txt, read it and prints it with Hi
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
+
+# 3) Code that opens numbers.txt, reads the first two numbers, then adds them together and print the sum
+with open("numbers.txt", "r") as in_file:
+    number1 = int(in_file.readline())
+    number2 = int(in_file.readline())
+print(number1 + number2)
