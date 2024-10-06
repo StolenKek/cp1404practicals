6445705efc28c9fa656383092aa2f46660034f54
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-06 12:28:30 +0800

    Edited the price range minimum, maximum and price increase; Updated program to write outputs to a file.

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -10,14 +10,17 @@ The price should be displayed to the nearest cent (e.g. $33.59, not $33.59182329
 """
 import random
 
-MAX_INCREASE = 0.1  # 10%
+MAX_INCREASE = 0.175  # 17.5%
 MAX_DECREASE = 0.05  # 5%
-MIN_PRICE = 0.01
-MAX_PRICE = 1000.0
+MIN_PRICE = 1.00
+MAX_PRICE = 100.00
 INITIAL_PRICE = 10.0
+FILENAME = "stock_price_simulator.txt"  # Output file name
 
 price = INITIAL_PRICE
 day = 0
+
+out_file = open(FILENAME, 'w')
 
 print(f"Starting price: ${price:,.2f}")
 
@@ -37,4 +40,6 @@ while MIN_PRICE <= price <= MAX_PRICE:
 
     price *= (1 + price_change)
 
-    print(f"On day {day} price is: ${price:,.2f}")
+    print(f"On day {day} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
