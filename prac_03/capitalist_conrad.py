00c2c9692de30eb1385b4380c1d82f5a7bb30f49
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-06 11:48:27 +0800

    Added the day count and sentences to the printed results

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -17,10 +17,13 @@ MAX_PRICE = 1000.0
 INITIAL_PRICE = 10.0
 
 price = INITIAL_PRICE
-print(f"${price:,.2f}")
+day = 0
+
+print(f"Starting price: ${price:,.2f}")
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
+    day += 1
     # generate a random integer of 1 or 2
     # if it's 1, the price increases, otherwise it decreases
     if random.randint(1, 2) == 1:
@@ -33,4 +36,5 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+
+    print(f"On day {day} price is: ${price:,.2f}")
