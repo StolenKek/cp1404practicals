e99baa712272a49f213db358fbd7e98494b43f75
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-10 18:16:49 +0800

    initial commit

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,8 @@
+"""
+CP1404/CP5632 - Practical
+Warming up on using lists
+
+What values do the following expressions have? Without running the code, record your answers to these questions in your Python file as a comment.
+Then use the Python console to see if you were correct.
+"""
+
