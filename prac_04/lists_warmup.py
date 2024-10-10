cbd0e340332a226ecee12ba891438eb22ffd504e
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-10 19:01:36 +0800

    changed the values of the first and last elements and printed certain results

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -25,5 +25,15 @@ numbers[3:4] # The value is 1, which is the number starting at position 3 and en
 "3" in numbers # False, because it does not exist in the list as a string, but as an integer
 numbers + [6, 5, 3] # The values are 3, 1, 4, 1, 5, 9, 6, 5, 3 which is everything in the list, but now 6, 5, 3 are added to it
 
+# Change the first element of numbers to "ten" (the string, not the number 10)
+numbers[0] = "ten"
 
+# Change the last element of numbers to 1
+numbers[-1] = 1
+
+# Print all the elements from numbers except the first two (slice)
+print(numbers[2:])
+
+# Print whether 9 is an element of numbers
+print(9 in numbers)
 
