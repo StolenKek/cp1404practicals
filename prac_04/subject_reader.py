088cbbc8ee98ec952978ff61d147a7f79c160f7b
StolenCakes <nicoleyiqinxianyiqin.sin@my.jcu.edu.au>
2024-10-10 22:34:06 +0800

    edited the display function to be less complex, and edited the load data to show only the necessary lines and return subject

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -28,31 +28,27 @@ CP1404 is taught by Alan Turing  and has  98 students
 
 FILENAME = "subject_data.txt"
 
-
 def main():
-    data = load_data()
-    print(data)
-
+    """Read the data and display using the format."""
+    subjects = load_data()
+    display_subjects(subjects)
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(FILENAME)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        subject.append(parts)
     input_file.close()
     return subject
 
 def display_subjects():
     """Display subject details formatted like: subject + is taught by + lecturer + and has + number of students"""
     for subject in subjects:
-        print(f"{subject[0]} is taught by {subject[1]:12} and has {subject[2]:3} students")
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
 
 main()
 
